9867816d7143e029cc653598faa39b5638225c70
Jens Getreu <getreu@web.de>
2025-11-16 04:26:34 +0200

    Follow `vial-qmk` chnages

--- totem/keymaps/vial/config.h
+++ totem/keymaps/vial/config.h
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0x62, 0xDB, 0xC0, 0x05, 0x7F, 0x34, 0xDD, 0x20}
+#define VIAL_KEYBOARD_UID {0x80, 0xD7, 0x5F, 0xD9, 0x25, 0x60, 0xAB, 0x3A}
 
 #define VIAL_UNLOCK_COMBO_ROWS \
     { 0, 4 }
@@ -20,3 +20,4 @@
 #undef LOCKING_RESYNC_ENABLE
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
--- totem/keymaps/vial/rules.mk
+++ totem/keymaps/vial/rules.mk
@@ -1,6 +1,10 @@
 VIA_ENABLE  = yes
 VIAL_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
 DYNAMIC_KEYMAP_LAYER_COUNT = 16
+SPLIT_KEYBOARD = yes
 
+# Enable features
+TAP_DANCE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+MACRO_ENABLE = yes         # standard QMK macros
+DYNAMIC_MACRO_ENABLE = yes # dynamic macros (record on the keyboard)
