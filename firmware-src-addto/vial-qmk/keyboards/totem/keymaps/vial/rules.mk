fa68da61626303db1da6355a544147ef4a1d8c5d
Jens Getreu <getreu@web.de>
2025-11-17 22:35:46 +0200

    Add Vial runtime config file

--- firmware-src-addto/vial-qmk/keyboards/totem/keymaps/vial/rules.mk
+++ firmware-src-addto/vial-qmk/keyboards/totem/keymaps/vial/rules.mk
@@ -1,10 +1,15 @@
-VIA_ENABLE  = yes
+LTO_ENABLE = yes
+VIA_ENABLE = yes
 VIAL_ENABLE = yes
+QMK_SETTINGS = yes
 DYNAMIC_KEYMAP_LAYER_COUNT = 16
 SPLIT_KEYBOARD = yes
 
 # Enable features
+COMBO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 MACRO_ENABLE = yes         # standard QMK macros
 DYNAMIC_MACRO_ENABLE = yes # dynamic macros (record on the keyboard)
+MOUSEKEY_ENABLE = yes
+
